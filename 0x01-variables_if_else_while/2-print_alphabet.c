ff2ddf4ef49b041a5be5c420a9a480b09b9842da
Pere Oroso <pereoroso@gmail.com>
2022-12-15 01:42:39 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,10 +11,12 @@
 int main(void)
 {
 	char low;
-	
+
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	        putchar('\n');
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
 
 	return (0);
 }
